f3b4e693f1ea77d1330b1aec20112881399ecdd7
Mark Veszelka <120094709+markveszelka@users.noreply.github.com>
2025-01-12 19:00:59 +0100

    Update CircleCI config file
    - Working pipeline
    - Update Chrome option arguments in environment.py

--- features/environment.py
+++ features/environment.py
@@ -6,7 +6,12 @@ from selenium.webdriver.chrome.service import Service
 def before_all(context):
     chrome_service = Service(ChromeDriverManager().install())
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("--start-maximized")
+    # Various arguments to make the browser headless
+    # chrome_options.add_argument("--start-maximized")
+    # chrome_options.add_argument('--no-sandbox')
+    # chrome_options.add_argument('--window-size=1420,1080')
+    # chrome_options.add_argument('--headless')
+    # chrome_options.add_argument('--disable-gpu')
     context.driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
     context.driver.implicitly_wait(10)
 
